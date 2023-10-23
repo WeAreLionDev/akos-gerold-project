122e3d465b2b8b3b608b266ad4f8358c81715919
Stefan Stevic <steviss@gmail.com>
2023-10-23 14:34:48 +0200

    feat: [AKO-37] add sanity setup and installation

---
+++ sanity/schemas/post.ts
@@ -0,0 +1,61 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+    defineField({
+      name: "subtitle",
+      title: "Subtitle",
+      description:
+        "Contents of this field appear on the HomePage under the title.",
+      type: "string",
+    }),
+    defineField({
+      name: "featuredImage",
+      title: "Featured image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [{ type: "reference", to: { type: "tag" } }],
+    }),
+    defineField({
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      media: "mainImage",
+    },
+  },
+});
