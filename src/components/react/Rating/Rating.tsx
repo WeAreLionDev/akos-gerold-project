c59c7501d5b49e8d144f5445981662369427c0be
Stefan Stevic <steviss@gmail.com>
2023-10-24 04:51:40 +0200

    refactor: [AKO-10] added font shard solid, fix ratings

--- src/components/react/Rating/Rating.tsx
+++ src/components/react/Rating/Rating.tsx
@@ -1,18 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as faStarRegular } from '@fortawesome/sharp-regular-svg-icons'
 import { faStar, faStarHalfStroke } from '@fortawesome/sharp-solid-svg-icons'
 import type { FC } from 'react'
 
 import type { RatingProps } from './Rating.interface'
+import { calculateRating } from './Rating.utility'
 
-const Rating: FC<RatingProps> = ({ subtitle, rating, maxRating }) => {
+const Rating: FC<RatingProps> = ({ subtitle, rating, maxRating = 5 }) => {
+  const fontAwesomeClasses = 'h-8 w-8 text-amber-300'
+  const { stars, halfStars, emptyStars } = calculateRating(rating, maxRating)
   return (
     <div className="flex flex-col items-center">
       <div className="mb-2 flex items-center gap-1">
-        <FontAwesomeIcon icon={faStar} className="h-8 w-8 text-yellow-400" />
-        <FontAwesomeIcon icon={faStar} className="h-8 w-8 text-yellow-400" />
-        <FontAwesomeIcon icon={faStar} className="h-8 w-8 text-yellow-400" />
-        <FontAwesomeIcon icon={faStar} className="h-8 w-8 text-yellow-400" />
-        <FontAwesomeIcon icon={faStarHalfStroke} className="h-8 w-8 text-yellow-400" />
+        {stars.map((star) => (
+          <FontAwesomeIcon key={`half-star-${star}`} icon={faStar} className={fontAwesomeClasses} />
+        ))}
+        {halfStars.map((halfStar) => (
+          <FontAwesomeIcon key={`half-star-${halfStar}`} icon={faStarHalfStroke} className={fontAwesomeClasses} />
+        ))}
+        {emptyStars.map((emptyStar) => (
+          <FontAwesomeIcon key={`half-star-${emptyStar}`} icon={faStarRegular} className={fontAwesomeClasses} />
+        ))}
         <span className="text-lg">
           {rating}/{maxRating}
         </span>
