916d00d8b173f3e0976d72aba71810d524962310
Stefan Stevic <steviss@gmail.com>
2023-10-25 18:57:53 +0200

    feat: [AKO-10] clients carousel fixes and animations

--- src/components/react/ClientCarousel/ClientCarousel.tsx
+++ src/components/react/ClientCarousel/ClientCarousel.tsx
@@ -1,19 +1,41 @@
-import type { FC } from 'react'
+import { motion, useAnimationControls } from 'framer-motion'
+import { type FC, useRef } from 'react'
 
 import './ClientCarousel.css'
 import type { ClientCarouselProps } from './ClientCarousel.interface'
 
-const ClientCarousel: FC<ClientCarouselProps> = ({ ...props }) => {
+const ClientCarousel: FC<ClientCarouselProps> = () => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const animationControls = useAnimationControls()
+  const onReset = () => {
+    animationControls.start({
+      x: 0,
+      y: 0,
+    })
+  }
+
   return (
-    <div className="flex flex-col">
-      <div {...props} className="client-section">
-        {Array.from(Array(8)).map(() => (
-          <>
-            <div className="bg-black" />
-            <div className="bg-red-500" />
-            <div className="bg-blue-500" />
-          </>
+    <div ref={containerRef} className="flex w-full max-w-sm flex-col xl:max-w-7xl">
+      <motion.div
+        className="client-section self-center"
+        drag="x"
+        animate={animationControls}
+        dragConstraints={containerRef}
+        dragElastic={1}
+        onDragEnd={onReset}
+      >
+        {Array.from(Array(8)).map((id) => (
+          <div key={`${id}-rd`} className="bg-red-500" />
         ))}
+        {Array.from(Array(8)).map((id) => (
+          <div key={`${id}-bl`} className="bg-blue-500" />
+        ))}
+        {Array.from(Array(8)).map((id) => (
+          <div key={`${id}-blc`} className="bg-black" />
+        ))}
+      </motion.div>
+      <div className="client-scrollbar">
+        <motion.div />
       </div>
     </div>
   )
