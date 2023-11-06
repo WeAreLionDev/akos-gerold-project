a55e77d4646699f54b3a22024f289d222f101c86
Stefan Stevic <steviss@gmail.com>
2023-11-06 22:53:35 +0100

    refactor: [AKO-45] testimonials carousel refactor, testimonial service refactor

--- src/components/react/Testimonial/Testimonial.tsx
+++ src/components/react/Testimonial/Testimonial.tsx
@@ -3,15 +3,15 @@ import type { FC } from 'react'
 import './Testimonial.css'
 import type { TestimonialProps } from './Testimonial.interface'
 
-const Testimonal: FC<TestimonialProps> = ({ author, avatar, body }) => {
+const Testimonal: FC<TestimonialProps> = ({ author, avatar, position, body }) => {
   return (
     <div className="testimonial-container">
       <div className="testimonial">
-        <div className="mb-5 flex max-h-[100px] max-w-[100px] flex-col">
+        <div className="mb-5 flex max-h-[100px] max-w-[100px] flex-col overflow-hidden rounded-full">
           <img src={avatar} alt={author} className="block h-auto w-full" />
         </div>
         <h3 className="mb-2 font-poppins text-xl font-semibold">{author}</h3>
-        <h4 className="mb-5 font-poppins text-base">Vice President of Sales Team BMW 123</h4>
+        <h4 className="mb-5 font-poppins text-base">{position}</h4>
         <p className="font-base text-center">“{body}”</p>
       </div>
     </div>
--- src/components/react/TestimonialBlock/TestimonialBlock.tsx
+++ src/components/react/TestimonialBlock/TestimonialBlock.tsx
@@ -3,11 +3,19 @@ import type { FC } from 'react'
 
 import type { TestimonialBlockProps } from './TestimonialBlock.interface'
 
-const TestimonialBlock: FC<TestimonialBlockProps> = ({ body, author, className, ...props }) => {
+const TestimonialBlock: FC<TestimonialBlockProps> = ({ body, author, position, avatar, className, ...props }) => {
   return (
     <div {...props} className={classNames(className, 'flex max-w-xs flex-col border p-3')}>
       <p className="mb-5 text-sm font-medium">“{body}”</p>
-      <h3 className="font-poppins text-base font-semibold">{author}</h3>
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <h3 className="font-poppins text-base font-semibold">{author}</h3>
+          <h4 className="font-poppins text-sm">{position}</h4>
+        </div>
+        <div className="flex max-h-[50px] max-w-[50px] flex-col overflow-hidden rounded-full">
+          <img src={avatar} alt={author} className="block h-auto w-full" />
+        </div>
+      </div>
     </div>
   )
 }
